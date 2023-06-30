401e2d4ab9a74d3f651a60c31bec6cf9b39dc1c9
sivan <sivan12304@gmail.com>
2023-06-30 07:58:23 +0300

    complete

--- frontend/src/Components/app/app.tsx
+++ frontend/src/Components/app/app.tsx
@@ -1,72 +1,116 @@
 import photosService from "../../Services/PhotoService";
-import "./app.css"
-import  { useState } from 'react';
+import "./app.css";
+import { useState } from "react";
 import { photoStore } from "../../Redux/PhotoState";
 
 
 function App(): JSX.Element {
-    //show all the photos
-    const [photo, setUsePhoto] = useState<any[]>([]);
-    //show the current Page
-    const [currentPage, setCurrentPage] = useState(1);
-    //show categories
-    const [cat, setUseCat] = useState<any[]>([]);
+  // show all the photos
+  const [photo, setUsePhoto] = useState<any[]>([]);
+  // show the current Page
+  const [currentPage, setCurrentPage] = useState(1);
+  // show categories
+  const [cat, setUseCat] = useState<any[]>([]);
 
-      const changeCategory = () => {
-        photosService.getCategories()
-        .then(cat => setUseCat(cat))
-        .catch(err => alert(err.message));
-      };
+  const changeCategory = () => {
+    photosService
+      .getCategories()
+      .then((cat) => setUseCat(cat))
+      .catch((err) => alert(err.message));
+  };
 
-      const handlePrevClick = () => {
-        if (currentPage > 1) {
-          setCurrentPage(currentPage - 1);
-          const type = photoStore.getState().image.category;
-          photosService.getPhotosByCategory(type, currentPage-1)
-          .then(p =>setUsePhoto(p.hits))
-          .catch(err => alert(err.message));
-        }
-      };
-    
-      const handleNextClick = () => {
-        setCurrentPage(currentPage + 1);
-        const type = photoStore.getState().image.category;
-        photosService.getPhotosByCategory(type, currentPage+1)
-        .then(p =>setUsePhoto(p.hits))
-        .catch(err => alert(err.message));
-      };
-
-      function choose(c:string) {
-        setCurrentPage(1);
-        photosService.getPhotosByCategory(c,currentPage)
-        .then(p =>setUsePhoto(p.hits) )
-        .catch(err => alert(err.message));
+  const handlePrevClick = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+      const type = photoStore.getState().image.category;
+      photosService
+        .getPhotosByCategory(type, currentPage - 1)
+        .then((p) => setUsePhoto(p.hits))
+        .catch((err) => alert(err.message));
     }
-    return (
-        <div className="app">
-          <h1>PHOTO-GALLERY/ sivan saban</h1>
-        <button type="button" className="btn btn-secondary btn-lg" onClick={()=>{changeCategory()}}>
-          Category
-        </button>
-        <br/><br/>
-        {cat.map((c) =>
-          <button type="button" className="btn btn-info" key={c} onClick={()=>choose(c)}> 
-            {c} 
-          </button> 
-        )}
-        <br/><br/>
-        <button type="button" className="btn btn-secondary" disabled={currentPage === 1} onClick={handlePrevClick}>prev</button>
-        <button type="button" className="btn btn-secondary" onClick={handleNextClick}>next</button>
-        <p>Current Page: {currentPage}</p>
+  };
 
-        {photo.map((photo) => 
-          <img
-            key={photo.id}
-            src={photo.previewURL}
-          />
-        )}
-        </div>
-    );
+  const handleNextClick = () => {
+    setCurrentPage(currentPage + 1);
+    const type = photoStore.getState().image.category;
+    photosService
+      .getPhotosByCategory(type, currentPage + 1)
+      .then((p) => setUsePhoto(p.hits))
+      .catch((err) => alert(err.message));
+  };
+
+  const choose = (c: string) => {
+    setCurrentPage(1);
+    photosService
+      .getPhotosByCategory(c, currentPage)
+      .then((p) => setUsePhoto(p.hits))
+      .catch((err) => alert(err.message));
+  };
+
+  const handleClick = (photo: any) => {
+    const newWindow = window.open("", "_blank", "width=600,height=400");
+    newWindow.document.write(`
+      <h2>Selected Photo:</h2>
+      <p>ID: ${photo.id}</p>
+      <p>URL: ${photo.previewURL}</p>
+      <p>views: ${photo.views}</p>
+      <p>downloads: ${photo.downloads}</p>
+      <p>collection: ${photo.collections}</p>
+      <p>likes: ${photo.likes}</p>
+    `);
+  };
+
+  return (
+    <div className="app">
+      <h1>PHOTO-GALLERY/ sivan saban</h1>
+      <button
+        type="button"
+        className="btn btn-secondary btn-lg"
+        onClick={changeCategory}
+      >
+        Categories
+      </button>
+      <br />
+      <br />
+      {cat.map((c) => (
+        <button
+          type="button"
+          className="btn btn-info"
+          key={c}
+          onClick={() => choose(c)}
+        >
+          {c}
+        </button>
+      ))}
+      <br />
+      <br />
+      <button
+        type="button"
+        className="btn btn-secondary"
+        disabled={currentPage === 1}
+        onClick={handlePrevClick}
+      >
+        prev
+      </button>
+      <button
+        type="button"
+        className="btn btn-secondary"
+        onClick={handleNextClick}
+      >
+        next
+      </button>
+      <p>Current Page: {currentPage}</p>
+
+    {photo.map((photo) => (
+        <img
+          key={photo.id}
+          src={photo.previewURL}
+          alt={photo.previewURL}
+          onClick={() => handleClick(photo)} // Add onClick event and call handleClick function
+        />
+      ))}
+    </div>
+  );
 }
 
 export default App;
